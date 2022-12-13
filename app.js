a7bc3a3925d14b6cd7fb581137fc25117a1aa018
wissemsamtouni <sa.wissem.samtouni@gmail.com>
2022-12-13 15:28:33 +0100

    final

--- app.js
+++ app.js
@@ -53,12 +53,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-<<<<<<< HEAD
+
 app.use('/public',express.static(path.join(__dirname, 'public')));
-=======
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bonplans', bonplansRouter);
->>>>>>> 018b96367758783a9a16c5572e2c37dc7ba88cce
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categorie', categorieRouter);
